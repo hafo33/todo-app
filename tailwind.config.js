a9c401766a13ca72891570a233ca25693f7ed7ba
hafo33 <hafo33@gmail.com>
2024-05-20 17:04:50 -0500

    initial commit setup tailwind css and basic html

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{html,js}', './public/index.html'],
+  darkmode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants : {
+    extend: {},
+  },
+  plugins: [],
+}
+
